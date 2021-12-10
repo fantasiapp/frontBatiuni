b12894548470105aeb1399f77dfd3e4fee8f5ff8
AnasCHATOUI <anaschatoui1997@gmail.com>
2021-12-10 16:11:22 +0100

    Dropdown with search and multiple choices

--- src/components/options/options.ts
+++ src/components/options/options.ts
@@ -7,11 +7,42 @@ import { Component } from "@angular/core";
 })
 export class OptionsModel {
   search : string = ''
-  Options = ['Anas','Hamza','Majed']
-  filtred: string[] = this.Options
+  showDropDown : Boolean =false;
+  Options = [
+    {id:1,job:"TCE", isChecked:false},
+    {id:2,job:"Cuisiniste" , isChecked:false},
+    {id:3,job:"Ingénieur en Aménagement et Urbanisme" , isChecked:false},
+    {id:4,job:"Ingénieur d'affaires du BTP" , isChecked:false},
+    {id:5,job:"Economiste de la construction", isChecked:false} ,
+  ]
+  
+  optionsSearch  = this.Options
+  userChoices : jobs[] = []
   filterOptions(e: Event) {
-    
     let search:string = (e.target as any).value
-    this.filtred = this.Options.filter(option=>option.toLowerCase().includes(search.toLowerCase()))
+  
+    this.optionsSearch = this.Options.filter(option=>option.job.toLowerCase().includes(search.toLowerCase()))
 }
+  added(e: Event,id:number) {
+    if((e.target as any).checked){
+      this.Options[id-1].isChecked = true;
+      let listtt = this.Options.filter(option => option.id == id)
+      this.userChoices.push(listtt[0])
+      // this.optionsSearch = this.optionsSearch.filter(option=>option.id != id); 
+      // this.Options = this.Options.filter(option=>option.id != id); 
+      }
+      if(!(e.target as any).checked){
+        this.userChoices = this.userChoices.filter(option => option.id != id)
+      }
+  }
+  delete(e:Event, id:number) {
+    this.Options[id-1] = {...this.Options[id-1], isChecked:false}
+    let removed = this.userChoices.filter(option => option.id == id)
+    this.userChoices = this.userChoices.filter(option => option.id != id)
+  }
 };
+interface jobs{
+  id: number 
+  job: string
+  isChecked:boolean
+}
