a18006fe88fc9f70e607ee497b34bc3575f49a6b
AnasCHATOUI <anaschatoui1997@gmail.com>
2021-12-29 11:06:34 +0100

    Abonnement carouseel scroll

--- src/components/abonnement/abonnement.page.ts
+++ src/components/abonnement/abonnement.page.ts
@@ -7,6 +7,23 @@ import { Component } from "@angular/core";
 export class AbonnementPage {
     activeView: number = 0;
 
-    
+    scrollLeft() {
+        let scroll = document.getElementsByClassName('caroussel')
+        let offset = scroll[0];
+        scroll[0].scrollTo({
+            top:0,
+            left:300 + scroll[0].scrollLeft,
+            behavior:'smooth'
+        })
+    }
+    scrollRight() {
+        let scroll = document.getElementsByClassName('caroussel')
+        let offset = scroll[0];
+        scroll[0].scrollTo({
+            top:0,
+            left: -300 + scroll[0].scrollLeft,
+            behavior:'smooth'
+        })
+    }
 
 }
