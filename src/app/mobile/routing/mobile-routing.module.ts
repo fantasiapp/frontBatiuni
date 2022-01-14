096bd1834d1f13cd661426db8b9101fc81efa9f0
noctisdark <diseven@outlook.fr>
2022-01-14 17:37:36 +0100

    send correct token

--- src/app/mobile/routing/mobile-routing.module.ts
+++ src/app/mobile/routing/mobile-routing.module.ts
@@ -48,7 +48,7 @@ const routes: Routes = [{
     component: RegisterComponent,
     data: { animation: 'Register' }
   }, {
-    path: 'confirmed/:token?',
+    path: 'confirmed/:token',
     component: EmailConfirmation,
     data: { animation: 'Confirmed' }
   }, {
