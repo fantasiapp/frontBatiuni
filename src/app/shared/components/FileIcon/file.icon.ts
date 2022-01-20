246414283d891d8d1e1a15d23b316df1ebf8db49
noctisdark <diseven@outlook.fr>
2022-01-20 11:51:27 +0100

    better file icons

--- src/app/shared/components/FileIcon/file.icon.ts
+++ src/app/shared/components/FileIcon/file.icon.ts
@@ -19,9 +19,9 @@ import { shadeColor } from "src/common/functions";
         <g transform="matrix(1, 0, 0, 1, -1.37, -6)" filter="url(#box-shadow)">
           <rect data-name="container" width="60.198" height="84.748" rx="7" transform="translate(9 15)" fill="#fff"/>
         </g>
-        <path data-name="filename arrow"  d="M6000.1,11956.382l7.747,8.433v-8.433Z" transform="translate(-6000.102 -11922.76)" [style.fill]="arrowColor"/>
+        <path data-name="filename arrow"  d="M6000.1,11956.382l7.747,8.433v-8.433Z" transform="translate(-6000.102 -11917.76)" [style.fill]="arrowColor"/>
         <g transform="translate(0, 20)">
-          <rect width="65" height="20" rx="5" [style.fill]="color"/>
+          <rect width="65" height="20" rx="2" [style.fill]="color"/>
           <text style="fill: #fff; z-index: 1" x="6" y="14" font-size="12.5" font-family="Poppins" font-weight="500">{{ name }}</text>
         </g>
         <rect data-name="Line 1" width="40" height="2.5" transform="translate(20.179 53.452)" fill="#ccc"/>
