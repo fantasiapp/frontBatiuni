30ecd2d41f26fffd456045325c8d51cb92323bff
jlw <jlw@Mac-mini-de-Jean-Luc.local>
2022-03-23 11:21:07 +0100

    update task

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  backUrl: 'https://batiuni.fantasiapp.tech:5002',
-  // backUrl: 'http://localhost:8000',
+  // backUrl: 'https://batiuni.fantasiapp.tech:5002',
+  backUrl: 'http://localhost:8000',
   version: 'temp'
 };
 
