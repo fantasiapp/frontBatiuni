93e7d32f9362d68174596e828b8a3ffce9115111
jlw <jlw@Mac-mini-de-Jean-Luc.local>
2022-04-14 10:35:53 +0200

    environnement.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,6 @@
 export const environment = {
     production: false,
-<<<<<<< HEAD
     // backUrl: 'https://batiuni.fantasiapp.tech:5004',
-=======
-    // backUrl: 'https://batiuni.fantasiapp.tech:5001',
->>>>>>> 64606ab9f4519e374226af9376f5051c88909161
     backUrl: 'http://localhost:8000',
     version: 'work'
   };
