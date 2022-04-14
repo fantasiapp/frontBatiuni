d2054db390925cf5b44c5678e03fbad308db1a4f
jlw <jlw@Mac-mini-de-Jean-Luc.local>
2022-04-14 09:25:40 +0200

    update version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,7 @@
 export const environment = {
     production: false,
-<<<<<<< HEAD
-    // backUrl: 'https://batiuni.fantasiapp.tech:5004',
-    backUrl: 'http://localhost:8000',
-=======
     backUrl: 'https://batiuni.fantasiapp.tech:5001',
     // backUrl: 'http://localhost:8000',
->>>>>>> 74296d124cc9a3b2f17727ff6ed9d6b7d559a3f8
     version: 'work'
   };
   
