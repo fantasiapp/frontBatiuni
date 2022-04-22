10f7be2b31efe1be02c22de675647c43190d478f
Lucas TRAN <lucas.tran@imt-atlqntique.net>
2022-04-22 10:15:46 +0200

    fix merge

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,6 @@
 export const environment = {
-<<<<<<< HEAD
-    production: false,
-    backUrl: 'https://batiuni.fantasiapp.tech:5002',
-    // backUrl: 'http://localhost:8000',
-    version: 'work'
-  };
-  
-=======
   production: false,
   backUrl: 'https://batiuni.fantasiapp.tech:5001',
   // backUrl: 'http://localhost:8000',
   version: 'work'
 };  
->>>>>>> 0479631faa98eb50071b04f49689bdbb7c5446ee
