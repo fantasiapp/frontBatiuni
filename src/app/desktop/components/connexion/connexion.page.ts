48dccf9f3a72ad2124a4aaf2f4891d56e801da05
Lucas TRAN <lucas.tran@imt-atlqntique.net>
2022-04-25 18:22:20 +0200

    connextion.page.scss naming

--- src/app/desktop/components/connexion/connexion.page.ts
+++ src/app/desktop/components/connexion/connexion.page.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 @Component({
   selector: 'connexion',
   templateUrl: './connexion.page.html',
-  styleUrls: ['./connexion.page .scss'],
+  styleUrls: ['./connexion.page.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ConnexionPageComponent {
