38ba8375a88a94e03a1f6939a4fcacdc476d1a63
MaximeBrt <maxime.baraton92@gmail.com>
2022-05-11 18:38:56 +0200

    test postuler
    - bug sur le format de PostFields

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   backUrl: "https://batiuni.fantasiapp.tech:5002",
   // backUrl: 'http://localhost:8000',
-  version: "work",
+  version: "current",
 };
