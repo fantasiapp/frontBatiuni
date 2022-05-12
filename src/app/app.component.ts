6c4d08091052d0c91490aa4cf2cc67aa026e0e35
MaximeBrt <maxime.baraton92@gmail.com>
2022-05-12 18:27:58 +0200

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,7 +64,7 @@ export class AppComponent extends Destroy$ {
   }
 
   async updateUserData() {
-    while (true) {
+    while (false) {
       console.log(
         "changeReadyToUpdate",
         this.changeReadyToUpdate(false),
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   backUrl: "https://batiuni.fantasiapp.tech:5002",
   // backUrl: 'http://localhost:8000',
-  version: "current",
+  version: "temp",
 };
