fc17872347f8f0fe534ebf4b5f2ecb08725d4fa7
Lucas TRAN <lucas.tran@imt-atlqntique.net>
2022-05-13 16:48:09 +0200

    mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,6 +40,19 @@ export class AppComponent extends Destroy$ {
   constructor(private store: Store, private mobile: Mobile) {
     super();
     this.mobile.init();
+
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // We listen to the resize event
+    // window.addEventListener('resize', () => {
+    //   // We execute the same script as before
+    //   let vh = window.innerHeight * 0.01;
+    //   document.documentElement.style.setProperty('--vh', `${vh}px`);
+    // });
   }
 
   ready$ = new AsyncSubject<true>();
@@ -112,3 +125,4 @@ export class AppComponent extends Destroy$ {
   //   }
   // }
 }
+
