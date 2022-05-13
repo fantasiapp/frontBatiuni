74a47f80cd288b16da625fd892aee206b8ba46b1
wlorgere <williamlorgere@gmail.com>
2022-05-13 10:56:41 +0200

    add boosted post border

--- src/app/mobile/components/booster/booster.page.ts
+++ src/app/mobile/components/booster/booster.page.ts
@@ -1,4 +1,5 @@
-import { Component,} from "@angular/core";
+import { Component, Input,} from "@angular/core";
+import { Post } from "src/models/new/data.interfaces";
 
 @Component({
   selector: 'booster-page',
@@ -7,4 +8,14 @@ import { Component,} from "@angular/core";
   
 })
 export class BoosterPage {
+
+  @Input()
+  post!: Post;
+
+
+
+  ngOnInit() {
+    console.log('booster page');
+    console.log(this.post);
+  }
 };
