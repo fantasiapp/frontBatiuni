441234af4faee06689e2ba2b0ee2e28c17b54ef1
wlorgere <williamlorgere@gmail.com>
2022-05-19 14:02:41 +0200

    fix environments.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,6 @@
 export const environment = {
   production: false,
-<<<<<<< HEAD
-  // backUrl: "https://batiuni.fantasiapp.tech:5002",
-  backUrl: 'http://localhost:8000',
-||||||| 9a32e3bb
   backUrl: "https://batiuni.fantasiapp.tech:5001",
   // backUrl: 'http://localhost:8000',
-=======
-  backUrl: "https://batiuni.fantasiapp.tech:5004",
-  // backUrl: 'http://localhost:8000',
->>>>>>> 6b0eb0fa0f1ec6093137478d9b378acd099bf322
   version: "current",
 };
