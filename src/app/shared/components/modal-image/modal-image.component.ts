591cfee8d65d73508c872723c63ac0389e5ea9db
wlorgere <williamlorgere@gmail.com>
2022-05-31 18:06:19 +0200

    mobile zoom

--- src/app/shared/components/modal-image/modal-image.component.ts
+++ src/app/shared/components/modal-image/modal-image.component.ts
@@ -18,6 +18,8 @@ export class ModalImage {
 
   scale: number = 1;
 
+  pinchScaling: boolean = false;
+
   constructor(private cd: ChangeDetectorRef) {
   }
   ngOnInit() {
@@ -55,54 +57,95 @@ export class ModalImage {
     };
 
     img.ontouchstart = (event) => {
-      event.preventDefault();
-      console.log("touchstart")
-      console.log(event)
-      
-      img = document.getElementById("target")!;
-      
-      this.xDown = event.touches[0].clientX;
-      this.yDown = event.touches[0].clientY;
+      if (event.touches.length === 2) {
+        this.pinchScaling = true;
+        
+      } else {
+        event.preventDefault();
+        console.log("touchstart")
+        console.log(event)
+        
+        img = document.getElementById("target")!;
+        
+        this.xDown = event.touches[0].clientX;
+        this.yDown = event.touches[0].clientY;
 
-      console.log("start", this.xDown, this.yDown)
+        console.log("start", this.xDown, this.yDown)
+      }
     };
 
     img.ontouchmove = (event) => {
-      event.preventDefault();
-      console.log("slide")
-      console.log(event);
+      if (this.pinchScaling) {
+        pinchMove(event);
+      } else {
+        event.preventDefault();
+        console.log("slide")
+        console.log(event);
 
-      let x = event.touches[0].clientX;
-      let y = event.touches[0].clientY;
+        let x = event.touches[0].clientX;
+        let y = event.touches[0].clientY;
 
-      let deltaX = x - this.xDown;
-      let deltaY = y - this.yDown;
+        let deltaX = x - this.xDown;
+        let deltaY = y - this.yDown;
 
-      console.log("move", deltaX, deltaY);
+        console.log("move", deltaX, deltaY);
 
-      img = document.getElementById("target")!;
+        img = document.getElementById("target")!;
 
-      console.log("scale move", this.scale)
-      console.log("move", this.xTranslation + deltaX, this.yTranslation + deltaY)
-      img.style.transform = `scale(${this.scale}) translate(${this.xTranslation + deltaX}px, ${this.yTranslation + deltaY}px)`;
+        console.log("scale move", this.scale)
+        console.log("move", this.xTranslation + deltaX, this.yTranslation + deltaY)
+        img.style.transform = `scale(${this.scale}) translate(${this.xTranslation + deltaX}px, ${this.yTranslation + deltaY}px)`;
+      }
     };
 
     img.ontouchend = (event) => {
-      event.preventDefault();
-      console.log("end")
-      console.log(event);
-      this.xTranslation += event.changedTouches[0].clientX - this.xDown;
-      this.yTranslation += event.changedTouches[0].clientY - this.yDown;
+      if (this.pinchScaling) {
+        pinchEnd(event);
+        this.pinchScaling = false;
+      } else {
+        event.preventDefault();
+        console.log("end")
+        console.log(event);
+        this.xTranslation += event.changedTouches[0].clientX - this.xDown;
+        this.yTranslation += event.changedTouches[0].clientY - this.yDown;
+      };
     };
 
-    img.addEventListener("gesturestart", (event) => {
-      event.preventDefault();
-      console.log("gesturestart")
-      console.log(event)
-      this.scale += 1 / 100;
-    });
   }
 
+  baseDist: number = 0;
+
+  getDistance(touch1: Touch, touch2: Touch) {
+    return Math.hypot(touch1.clientX - touch2.clientX, touch1.clientY - touch2.clientY);
+  }
+
+  pinchStart(event: TouchEvent) {
+    event.preventDefault();
+    console.log("pinch start")
+    console.log(event)
+    this.baseDist = this.getDistance(event.touches[0], event.touches[1]);
+  }
+
+  pinchMove(event: TouchEvent) {
+    event.preventDefault();
+    console.log("pinch")
+    console.log(event)
+    let dist = this.getDistance(event.touches[0], event.touches[1]);
+    console.log(dist);
+    
+    this.scale = this.scale * dist / this.baseDist;
+
+    let img = document.getElementById("target")!;
+    img.style.transform = `scale(${this.scale}) translate(${this.xTranslation}px, ${this.yTranslation}px)`;
+  }
+
+  pinchEnd(event: TouchEvent) {
+    event.preventDefault();
+    console.log("pinch end")
+    console.log(event)
+    this.baseDist = this.getDistance(event.changedTouches[0], event.changedTouches[1]);
+  }
+    
   async test() {
     let bool = true
     while(true) {
