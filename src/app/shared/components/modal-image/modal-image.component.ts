ebd01e83ad04e8f718c8ee0959b8edb71a2f8cc7
wlorgere <williamlorgere@gmail.com>
2022-05-31 18:36:20 +0200

    fix on pc

--- src/app/shared/components/modal-image/modal-image.component.ts
+++ src/app/shared/components/modal-image/modal-image.component.ts
@@ -124,75 +124,65 @@ export class ModalImage {
 
     this.hammerIt(img);
 
-    // img.onwheel = (event) => {
-    //   event.preventDefault();
-    //   console.log("zoom")
-    //   console.log(event)
-    //   console.log(this)
-    //   console.log("scale before", this.scale)
+    img.onwheel = (event) => {
+      event.preventDefault();
+      console.log("zoom")
+      console.log(event)
+      console.log(this)
+      console.log("scale before", this.scale)
 
-    //   this.scale += event.deltaY / 100;
-    //   this.scale = Math.max(1, Math.min(this.scale, 10));
-    //   console.log("scale after", this.scale);
+      this.scale += event.deltaY / 100;
+      this.scale = Math.max(1, Math.min(this.scale, 10));
+      console.log("scale after", this.scale);
 
-    //   img = document.getElementById("target")!;
-    //   img.style.transform = `scale(${this.scale}) translate(${this.xTranslation}px, ${this.yTranslation}px)`;
-    // };
+      img = document.getElementById("target")!;
+      img.style.transform = `translate(${this.xTranslation}px, ${this.yTranslation}px) scale(${this.scale})`;
+    };
 
-    // img.ontouchstart = (event) => {
-    //   if (event.touches.length === 2) {
-    //     this.pinchScaling = true;
-    //     this.pinchStart(event);
-    //   } else {
-    //     event.preventDefault();
-    //     console.log("touchstart")
-    //     console.log(event)
+    img.ontouchstart = (event) => {
+        event.preventDefault();
+        console.log("touchstart")
+        console.log(event)
         
-    //     img = document.getElementById("target")!;
+        img = document.getElementById("target")!;
         
-    //     this.xDown = event.touches[0].clientX;
-    //     this.yDown = event.touches[0].clientY;
+        this.xDown = event.touches[0].clientX;
+        this.yDown = event.touches[0].clientY;
 
-    //     console.log("start", this.xDown, this.yDown)
-    //   }
-    // };
+        console.log("start", this.xDown, this.yDown)
+      
+    };
 
-    // img.ontouchmove = (event) => {
-    //   if (this.pinchScaling) {
-    //     this.pinchMove(event);
-    //   } else {
-    //     event.preventDefault();
-    //     console.log("slide")
-    //     console.log(event);
+    img.ontouchmove = (event) => {
 
-    //     let x = event.touches[0].clientX;
-    //     let y = event.touches[0].clientY;
+        event.preventDefault();
+        console.log("slide")
+        console.log(event);
 
-    //     let deltaX = x - this.xDown;
-    //     let deltaY = y - this.yDown;
+        let x = event.touches[0].clientX;
+        let y = event.touches[0].clientY;
 
-    //     console.log("move", deltaX, deltaY);
+        let deltaX = x - this.xDown;
+        let deltaY = y - this.yDown;
 
-    //     img = document.getElementById("target")!;
+        console.log("move", deltaX, deltaY);
 
-    //     console.log("scale move", this.scale)
-    //     console.log("move", this.xTranslation + deltaX, this.yTranslation + deltaY)
-    //     img.style.transform = `scale(${this.scale}) translate(${this.xTranslation + deltaX}px, ${this.yTranslation + deltaY}px)`;
-    //   }
-    // };
+        img = document.getElementById("target")!;
 
-    // img.ontouchend = (event) => {
-    //   if (this.pinchScaling) {
-    //     this.pinchEnd(event);
-    //     this.pinchScaling = false;
-    //   } else {
-    //     event.preventDefault();
-    //     console.log("end")
-    //     console.log(event);
-    //     this.xTranslation += event.changedTouches[0].clientX - this.xDown;
-    //     this.yTranslation += event.changedTouches[0].clientY - this.yDown;
-    //   };
-    // };
+        console.log("scale move", this.scale)
+        console.log("move", this.xTranslation + deltaX, this.yTranslation + deltaY)
+        img.style.transform = `translate(${this.xTranslation + deltaX}px, ${this.yTranslation + deltaY}px) scale(${this.scale})`;
+      
+    };
+
+    img.ontouchend = (event) => {
+
+        event.preventDefault();
+        console.log("end")
+        console.log(event);
+        this.xTranslation += event.changedTouches[0].clientX - this.xDown;
+        this.yTranslation += event.changedTouches[0].clientY - this.yDown;
+      };
 
   }
   
