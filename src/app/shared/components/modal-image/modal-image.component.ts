e2add006895f1a02986dd4e9df66b6921e486658
wlorgere <williamlorgere@gmail.com>
2022-05-31 18:47:33 +0200

    fix mobile movement

--- src/app/shared/components/modal-image/modal-image.component.ts
+++ src/app/shared/components/modal-image/modal-image.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from "@angular/core";
 import { SafeResourceUrl } from "@angular/platform-browser";
+import { Capacitor } from "@capacitor/core";
 import { delay } from "../../common/functions";
 import { ImageGenerator } from "../../services/image-generator.service";
 
@@ -18,14 +19,8 @@ export class ModalImage {
 
   scale: number = 1;
 
-  pinchScaling: boolean = false;
 
-  constructor(private cd: ChangeDetectorRef) {
-  }
   ngOnInit() {
-    document.onreadystatechange = (state) => {
-      console.log("plop")
-    }
   }
 
 
@@ -35,6 +30,9 @@ export class ModalImage {
   xTranslation: number = 0;
   yTranslation: number = 0;
 
+  // This function comes from StackOverflow:
+  // https://stackoverflow.com/questions/18011099/pinch-to-zoom-using-hammer-js
+  // It is used to zoom and translate the image on mobile devices.
   hammerIt(elm: HTMLElement) {
     let hammertime = new Hammer(elm, {});
     hammertime.get('pinch').set({
@@ -117,73 +115,50 @@ export class ModalImage {
 }
 
   ngAfterViewInit() {
-    console.log("modal");
-    console.log(this.src);
     let img = document.getElementById("target")!;
-    console.log("div", img);
 
-    this.hammerIt(img);
+    if (Capacitor.getPlatform() === "web") {
+        img.onwheel = (event) => {
+            event.preventDefault();
 
-    img.onwheel = (event) => {
-      event.preventDefault();
-      console.log("zoom")
-      console.log(event)
-      console.log(this)
-      console.log("scale before", this.scale)
+            this.scale += event.deltaY / 100;
+            this.scale = Math.max(1, Math.min(this.scale, 10));
 
-      this.scale += event.deltaY / 100;
-      this.scale = Math.max(1, Math.min(this.scale, 10));
-      console.log("scale after", this.scale);
+            img = document.getElementById("target")!;
+            img.style.transform = `translate(${this.xTranslation}px, ${this.yTranslation}px) scale(${this.scale})`;
+        };
 
-      img = document.getElementById("target")!;
-      img.style.transform = `translate(${this.xTranslation}px, ${this.yTranslation}px) scale(${this.scale})`;
-    };
+        img.ontouchstart = (event) => {
+            event.preventDefault();
+            
+            img = document.getElementById("target")!;
+            
+            this.xDown = event.touches[0].clientX;
+            this.yDown = event.touches[0].clientY;
+        };
 
-    img.ontouchstart = (event) => {
-        event.preventDefault();
-        console.log("touchstart")
-        console.log(event)
+        img.ontouchmove = (event) => {
+            event.preventDefault();
+
+            let x = event.touches[0].clientX;
+            let y = event.touches[0].clientY;
+
+            let deltaX = x - this.xDown;
+            let deltaY = y - this.yDown;
+
+            img = document.getElementById("target")!;
+            img.style.transform = `translate(${this.xTranslation + deltaX}px, ${this.yTranslation + deltaY}px) scale(${this.scale})`;
         
-        img = document.getElementById("target")!;
-        
-        this.xDown = event.touches[0].clientX;
-        this.yDown = event.touches[0].clientY;
+        };
 
-        console.log("start", this.xDown, this.yDown)
-      
-    };
-
-    img.ontouchmove = (event) => {
-
-        event.preventDefault();
-        console.log("slide")
-        console.log(event);
-
-        let x = event.touches[0].clientX;
-        let y = event.touches[0].clientY;
-
-        let deltaX = x - this.xDown;
-        let deltaY = y - this.yDown;
-
-        console.log("move", deltaX, deltaY);
-
-        img = document.getElementById("target")!;
-
-        console.log("scale move", this.scale)
-        console.log("move", this.xTranslation + deltaX, this.yTranslation + deltaY)
-        img.style.transform = `translate(${this.xTranslation + deltaX}px, ${this.yTranslation + deltaY}px) scale(${this.scale})`;
-      
-    };
-
-    img.ontouchend = (event) => {
-
-        event.preventDefault();
-        console.log("end")
-        console.log(event);
-        this.xTranslation += event.changedTouches[0].clientX - this.xDown;
-        this.yTranslation += event.changedTouches[0].clientY - this.yDown;
-      };
-
+        img.ontouchend = (event) => {
+            event.preventDefault();
+            this.xTranslation += event.changedTouches[0].clientX - this.xDown;
+            this.yTranslation += event.changedTouches[0].clientY - this.yDown;
+        };
+    } else {
+        this.hammerIt(img);
+    }
   }
   
     
