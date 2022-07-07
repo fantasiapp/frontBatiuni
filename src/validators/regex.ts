c8ca9a48e79e7906cd1c03d33c7d94ae0d67aab4
wlorgere <williamlorgere@gmail.com>
2022-07-07 12:14:17 +0200

    change regex to check url

--- src/validators/regex.ts
+++ src/validators/regex.ts
@@ -4,4 +4,4 @@ export const number = /[0-9]/;
 export const symbols = /\+|\-|\*|\_|\,|\;|\./;
 export const email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const phone = /^(\+\d{1,3}|0|00\d{2})?[1-9]\d{8}$/;
-export const url = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/;
+export const url = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[\w_-]+(\.\w+)+((\/)[\w#]+)*(\/\w+\?[\w_]+=[\w\+\._-]+(&[\w_]+=[\w\+\._-]+)*)?(\/)?$/;
