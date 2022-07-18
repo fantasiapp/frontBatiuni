3b54461a081f80970b743cd942715517da0ae283
wlorgere <williamlorgere@gmail.com>
2022-07-18 14:55:08 +0200

    add subscriptions

--- src/app/stripeProducts.ts
+++ src/app/stripeProducts.ts
@@ -4,6 +4,8 @@ let temp = {
     boost3: "prod_M0BtJQKNIQBjvF",
     boost7: "prod_M0BuvRUprcxujw",
     boost0: "prod_M0BuUhllo4RnKr",
+    subscriptionST: "",
+    subscriptionPME: ""
   };
   switch (environment.backUrl[environment.backUrl.length - 1]) {
     case "1":
@@ -11,6 +13,8 @@ let temp = {
         boost3: "prod_M0BREvMmK263p4",
         boost7: "prod_M0BRA3MiH6A92x",
         boost0: "prod_M0BR3WcVYr4za8",
+        subscriptionST: "prod_M0wwP5ZavsuY90",
+        subscriptionPME: "prod_M0wLzHMEIAgYlP"
       };
       break;
     case "2":
@@ -18,6 +22,8 @@ let temp = {
         boost3: "prod_M0V2RzWQAHxQtX",
         boost7: "prod_M0V3Ao15zvtPjL",
         boost0: "prod_M0V3LGtyd1G8QX",
+        subscriptionST: "prod_M0wwP5ZavsuY90",
+        subscriptionPME: "prod_M0wLzHMEIAgYlP"
       };
       break;
     case "3":
@@ -25,6 +31,8 @@ let temp = {
         boost3: "prod_M0V7ybe5P6pl7Q",
         boost7: "prod_M0V7dhGlCfSemw",
         boost0: "prod_M0V8V6gkS4484n",
+        subscriptionST: "prod_M0wwP5ZavsuY90",
+        subscriptionPME: "prod_M0wLzHMEIAgYlP"
       };
       break;
     case "4":
@@ -32,6 +40,8 @@ let temp = {
         boost3: "prod_M0BtJQKNIQBjvF",
         boost7: "prod_M0BuvRUprcxujw",
         boost0: "prod_M0BuUhllo4RnKr",
+        subscriptionST: "prod_M0wwP5ZavsuY90",
+        subscriptionPME: "prod_M0wLzHMEIAgYlP"
       };
       break;
   }
