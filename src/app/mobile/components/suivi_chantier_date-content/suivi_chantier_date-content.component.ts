198e0f91e9f6c5e25a961bf294e2cf54811e987c
Nam Hai <lucastran00@gmail.com>
2022-07-20 14:49:42 +0200

    supervision main comment

--- src/app/mobile/components/suivi_chantier_date-content/suivi_chantier_date-content.component.ts
+++ src/app/mobile/components/suivi_chantier_date-content/suivi_chantier_date-content.component.ts
@@ -245,7 +245,7 @@ export class SuiviChantierDateContentComponent extends Destroy$ {
   
   swipePhoto?: {
     task: PostDetailGraphic | null,
-    taskGraphic: TaskGraphic
+    taskGraphic: TaskGraphic | null
   }
 
   async takePhoto() {
@@ -332,7 +332,7 @@ export class SuiviChantierDateContentComponent extends Destroy$ {
     }
   }
   
-  mainComment(task:PostDetailGraphic | null, taskGraphic: TaskGraphic, photo: Photo) {
+  mainComment(task:PostDetailGraphic | null, taskGraphic: TaskGraphic | null, photo: Photo) {
     if (!this.mission!.isClosed) {
       let detailPostId: number | null = task ? task.id : null
       let datePostId: number | null = null;
@@ -360,8 +360,6 @@ export class SuiviChantierDateContentComponent extends Destroy$ {
             supervisions[supervisions.length - 1] = this.store.selectSnapshot(DataQueries.getById('Supervision', supervisions[supervisions.length - 1].id))! 
             console.log('NEW SUPERVISION ', supervisions);
 
-            let postDetail = this.store.selectSnapshot(DataQueries.getById('DetailedPost', detailPostId!))!
-            console.log('postDetail', postDetail);
             if(detailPostId){
               this.date.postDetails.forEach(Detail =>{
                 console.log('detail', Detail);
