3177f13503229825939849efbc8ae194ddb7a309
Divya Urquijo <divya.urquijo@gmail.com>
2022-07-20 14:19:49 +0200

    bande blanche

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backUrl: "https://batiuni.fantasiapp.tech:5001",
+  backUrl: "https://batiuni.fantasiapp.tech:5003",
   // backUrl: 'http://localhost:8000',
   version: "current",
   firebase: {
